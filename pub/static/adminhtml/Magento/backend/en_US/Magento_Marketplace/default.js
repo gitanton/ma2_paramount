16f62a9c7d920ad6d8afc7a66455765d03ed6f24
andrey <andrey.g88@mail.com>
2017-02-17 00:19:26 -0600

    update pub static

--- pub/static/adminhtml/Magento/backend/en_US/Magento_Marketplace/default.js
+++ pub/static/adminhtml/Magento/backend/en_US/Magento_Marketplace/default.js
@@ -1,1 +1,20 @@
-/home2/magehire/public_html/para/vendor/magento/module-marketplace/view/adminhtml/web/default.js
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    return function (configFromPage) {
+        var url = configFromPage.url;
+
+        $.ajax({
+            method: 'GET',
+            url: url
+        }).done(function (data) {
+            $('div[data-role=partners-block]').html(data);
+        });
+    };
+});
